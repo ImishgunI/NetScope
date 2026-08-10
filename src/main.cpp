f528b3cc6f3af1f1081e6cb662539312aea7c48d
Michael <mvasilyev04@gmail.com>
2026-08-11 02:42:07 +0300

    Вызвал метод захвата кадров ethernet

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-int main() { return 0; }
+#include "./capture/capture.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: ./bin_file <ethernet_interface>";
+        return 1;
+    }
+    const char* interface_name = argv[1];
+    EthernetFrameCapturer::captureFrames(interface_name);
+    return 0;
+}
