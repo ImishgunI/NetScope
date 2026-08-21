8573b2696166dbf85d34e8da64aa6b2e6db27ba9
Michael <mvasilyev04@gmail.com>
2026-08-21 07:06:11 +0300

    Протестировал, что работает Capturer

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "./capture/capture.hpp"
+#include <array>
+#include <cstdint>
+#include <format>
 #include <iostream>
+
+void print(std::array<uint8_t, 4096>& arr) {
+    std::cout << std::format("{:02x}", arr[0]) << " " << std::format("{:02x}", arr[1]) << " "
+              << std::format("{:02x}", arr[2]) << " " << std::format("{:02x}", arr[3]) << " "
+              << std::format("{:02x}", arr[4]) << " " << std::format("{:02x}", arr[5]) << " "
+              << std::format("{:02x}", arr[6]) << " " << std::format("{:02x}", arr[7]) << " "
+              << std::format("{:02x}", arr[8]) << " " << std::format("{:02x}", arr[9]) << " "
+              << std::format("{:02x}", arr[10]) << " " << std::format("{:02x}", arr[11]) << " "
+              << std::format("{:02x}", arr[12]) << " " << std::format("{:02x}", arr[13]) << " "
+              << std::format("{:02x}", arr[14]) << " " << std::format("{:02x}", arr[15]) << '\n';
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -7,6 +21,7 @@ int main(int argc, char** argv) {
         return 1;
     }
     const char* interface_name = argv[1];
-    EthernetFrameCapturer::captureFrames(interface_name);
+    EthernetFrameCapturer enp11s0(interface_name);
+    enp11s0.captureLoop(print);
     return 0;
 }
